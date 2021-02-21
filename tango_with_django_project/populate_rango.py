673436736492c941ff55e19e8cdfeb2f4d64dcb1
Rico Tai <2471034t@student.gla.ac.uk>
2021-02-21 21:10:08 +0000

    Chapter 6 Completed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,4 +1,3 @@
-  
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 
@@ -12,29 +11,29 @@ def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/',
-         'views': 114,},
+         'views': 3131,},
         {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/',
-         'views': 53},
+         'views': 541},
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/',
-         'views': 20} ]
+         'views': 2313} ]
     
     django_pages = [
         {'title':'Official Django Tutorial',
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
-         'views': 32},
+         'views': 414},
         {'title':'Django Rocks',
          'url':'http://www.djangorocks.com/',
-         'views': 12},
+         'views': 31},
         {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/',
-         'views': 1258} ]
+         'views': 313} ]
     
     other_pages = [
         {'title':'Bottle',
          'url':'http://bottlepy.org/docs/dev/',
-         'views': 54},
+         'views': 313},
         {'title':'Flask',
          'url':'http://flask.pocoo.org',
          'views': 64} ]
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,3 +1,4 @@
+  
 from django.urls import path
 from rango import views
 
@@ -6,5 +7,6 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+        views.show_category, name='show_category'),
 ]
-
